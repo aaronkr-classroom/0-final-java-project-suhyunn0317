12c6dcef42cc1781255e6b5e27a55c9ec097fafb
suhyunn0317 <chojs3927@naver.com>
2024-03-12 17:22:31 +0900

    good

--- src/무제/Welcome.java
+++ src/무제/Welcome.java
@@ -1,9 +1,18 @@
-package 무제;
+import java.util.Scanner;
+
 
 public class Welcome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("당신의 이름을 입력하세요: ");
+		String name = input.nextLine();
+		System.out.println("연락처를 입력하세요: ");
+		int phone = input.nextInt();
+		System.out.println("Type your age: ");
+		int age = input.nextInt();
+		System.out.println("Your age: " + age);
 		String greeting = "Welocme to Shopping Mall!";
 	    String tagline = "Welcome to Book Market!!";
 		
@@ -17,6 +26,10 @@ public class Welcome {
 		System.out.println("3. 장바구니 비우\t7. 장바구니의 항목 삭제하기" );
 		System.out.println("4. 영수증 표시하\t8. 종료" );
 		System.out.println("*************************");
+		
+		System.out.println("메뉴 번호를 선택하세요: ");
+		int choice = input.nextInt();
+		System.out.println(choice+ "번을 선택했습니다.");
 	}
 
 }
